c5584f0d4ede8d30d816de5081c2a7b5abbfff0d
sarthak <sarthakS93@gmail.com>
2016-03-24 03:01:21 +0530

    recursion basics

--- recursion/basic.cpp
+++ recursion/basic.cpp
@@ -69,6 +69,111 @@ int checkIfReverse(char* s1, char* s2) {
 }
 
 
+void selectionSortHelper(int a[], int n, int index) {
+    if(index == n)
+        return;
+
+    int min = a[index], minPos = index;
+    for(int i = index + 1; i < n; i++) {
+        if(a[i] < min) {
+            min = a[i];
+            minPos = i;
+        }
+    }
+    swap(a[index], a[minPos]);
+
+    selectionSortHelper(a, n, index + 1);
+}
+
+void selectionSort() {
+    cout<<"Enter the size of the array: "<<endl;
+    int n, a[10];
+    cin>>n;
+    cout<<"Enter the elements of the array: "<<endl;
+    for(int i = 0; i < n; i++) {
+        cin>>a[i];
+    }
+
+    selectionSortHelper(a, n, 0);
+
+    cout<<"The sorted array is as follows: "<<endl;
+    for(int i = 0; i < n; i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+}
+
+void bubbleSortHelper(int a[], int end) {
+    if(end == 1)
+        return;
+    for(int i = 0; i < end - 1; i++) {
+        if(a[i] > a[i + 1])
+            swap(a[i], a[i + 1]);
+    }
+    bubbleSortHelper(a, end - 1);
+}
+
+
+void bubbleSort() {
+    cout<<"Enter the size of the array: "<<endl;
+    int n, a[10];
+    cin>>n;
+    cout<<"Enter the elements of the array: "<<endl;
+    for(int i = 0; i < n; i++) {
+        cin>>a[i];
+    }
+
+    bubbleSortHelper(a, n);
+
+    cout<<"The sorted array is as follows: "<<endl;
+    for(int i = 0; i < n; i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+}
+
+int powHelper(int x, int n) {
+    if(n == 0)
+        return 1;
+    return (x * powHelper(x, n - 1));
+}
+
+
+void pow() {
+    cout<<"Enter the base and exponent: "<<endl;
+    int x, n;
+    cin>>x>>n;
+    cout<<"Output: "<<powHelper(x, n)<<endl;
+}
+
+int removeXHelper(char* s, char* op) {
+    if(s[0] == '\0')
+        return 0;
+
+    int size = removeXHelper(s + 1, op);
+
+    if(s[0] == 'x' || s[0] == 'X') {
+        return size;
+    }
+    else {
+        op[size++] = s[0];
+        op[size] = '\0';
+        return size;
+    }
+}
+
+void removeX() {
+    cout<<"Enter the string: "<<endl;
+    char s[10], op[10];
+    cin>>s;
+    int size =  removeXHelper(s, op);
+    if(size == 0) {
+        cout<<"Output: "<<endl;
+        return;
+    }
+    reverseString(op, 0, size);
+    cout<<"Output: "<<op<<endl;
+}
+
+
 
 int main() {
 
@@ -88,6 +193,11 @@ cout<<++a<<endl;*/
 /*char s1[10] = "abc";
 char s2[10] = "dba";
 cout<<checkIfReverse(s1,s2)<<endl;*/
+
+//selectionSort();
+//bubbleSort();
+//pow();
+removeX();
 
 
 return 0;
